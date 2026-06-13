223c7825cbf9a265ba5ad511fed8b97f5e74c027
jhha-ux <jhha@pcalm.co.kr>
2026-06-13 20:29:07 +0900

    Update monitoring-banner.js

--- monitoring-banner.js
+++ monitoring-banner.js
@@ -9,7 +9,7 @@
 
 (function () {
   // ▼▼▼ 여기에 Apps Script 웹앱 배포 URL 붙여넣기 ▼▼▼
-  const WEBAPP_URL = 'YOUR_APPS_SCRIPT_WEBAPP_URL_HERE';
+  const WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbwVMAi8LVsiZbihW6U03beXh1u1QOya5p2o9NvtazfXwJ-lRfWL4hPx5ixuBdk48h91/exec';
   // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
 
   const css = `
